dde83fa3f70ea65433197b343be732c9e389a2d6
= <=nchaubau@gmail.com>
2019-08-09 09:27:26 +0700

    thao tac mang

---
+++ taothaotacvoimang.js
@@ -0,0 +1,17 @@
+let x = 0;
+let array =[];
+function add_element_to_array(){
+    array[x] =  document.getElementById("txtvalue").value;
+    alert("element"+array[x]+"added at index" + x );
+    x++;
+    document.getElementById("txtvalue").value = "";
+}
+function display_array(){
+    let e = "<hr/>";
+    for(let i = 0 ; i < array.length; i++){
+        e += "Element" + i + "  = " + array[i] +"<br/>";
+
+    }
+
+    document.getElementById("result").innerHTML = e;
+}
